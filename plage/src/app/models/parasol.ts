e3b5c2b720e2ad4e29a6c5873d45ebfc5fd14ade
Augustin Moinat <augustin.moinat@gmail.com>
2023-01-27 15:07:48 +0100

    Joli dessins

---
+++ plage/src/app/models/parasol.ts
@@ -0,0 +1,8 @@
+export class Parasol {
+    constructor(
+        public numEmplacement : number,
+        public numfile : number,
+        public reserve : boolean,
+        public select : boolean,
+    ){}
+}
---
+++ plage/src/app/planning/planning.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Parasol } from './../models/parasol';
+
+@Component({
+  selector: 'app-planning',
+  templateUrl: './planning.component.html',
+  styleUrls: ['./planning.component.css'],
+})
+export class PlanningComponent {
+  files:any[];
+  constructor() {
+    var file1 = [];
+    var file2 = [];
+    var file3 = [];
+    for (let i = 0; i < 10; i++) {
+      file1.push(new Parasol(i,1,true, false));
+      file2.push(new Parasol(i,2,false, false));
+      file3.push(new Parasol(i,3,true, false));
+    }
+    this.files=[file1,file2,file3];
+  }
+  select(parasol:Parasol){
+    if(!parasol.reserve){
+      parasol.select = !parasol.select;
+    }
+  }
+}
