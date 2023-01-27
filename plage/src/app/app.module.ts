aa9165ca1e8d5f7a6c51271ca078215e72bfa48f
Mathieu Couchaud <mathieucouchaud@gmail.com>
2023-01-27 16:28:52 +0100

    Update app.module.ts

--- plage/src/app/app.module.ts
+++ plage/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanningComponent } from './sub/planning/planning.component';
 import { AccueilComponent } from './main/accueil/accueil.component';
-import { InscriptionComponent } from './inscription/inscription.component';
+import { InscriptionComponent } from './main/inscription/inscription.component';
 import { NavbarComponent } from './sub/navbar/navbar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { LocataireComponent } from './locataire/locataire.component';
-import { LocataireResaComponent } from './locataire-resa/locataire-resa.component';
-import { ConcessionnaireComponent } from './concessionnaire/concessionnaire.component';
-import { LocataireVoirComponent } from './locataire-voir/locataire-voir.component';
-import { ConcessionnaireResaComponent } from './concessionnaire-resa/concessionnaire-resa.component';
-import { ConcessionnaireResaTraiterComponent } from './concessionnaire-resa-traiter/concessionnaire-resa-traiter.component';
-import { ConcessionnaireClientsComponent } from './concessionnaire-clients/concessionnaire-clients.component';
-import { ConcessionnaireUneResaComponent } from './concessionnaire-une-resa/concessionnaire-une-resa.component';
+import { LocataireComponent } from './main/locataire/locataire.component';
+import { LocataireResaComponent } from './main/locataire-resa/locataire-resa.component';
+import { ConcessionnaireComponent } from './main/concessionnaire/concessionnaire.component';
+import { LocataireVoirComponent } from './main/locataire-voir/locataire-voir.component';
+import { ConcessionnaireResaComponent } from './main/concessionnaire-resa/concessionnaire-resa.component';
+import { ConcessionnaireResaTraiterComponent } from './main/concessionnaire-resa-traiter/concessionnaire-resa-traiter.component';
+import { ConcessionnaireClientsComponent } from './main/concessionnaire-clients/concessionnaire-clients.component';
+import { ConcessionnaireUneResaComponent } from './main/concessionnaire-une-resa/concessionnaire-une-resa.component';
 
 @NgModule({
   declarations: [
