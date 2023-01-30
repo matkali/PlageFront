4fec1ff73dffe7b92a70d3815b5cab72bd0a0395
Mathieu Couchaud <mathieucouchaud@gmail.com>
2023-01-30 14:11:01 +0100

    Update locataire.ts

--- plage/src/app/models/locataire.ts
+++ plage/src/app/models/locataire.ts
@@ -1,5 +1,17 @@
-export class Locataire{
+import { Utilisateur } from './utilisateur';
+
+export class Locataire extends Utilisateur{
     constructor(
+        public override id : number,
+        public override email : string,
+        public override role : string,
         public dateHeure : Date,
-    ){}
+        public lienDeParente : string,
+        public pays : string,
+        public valide : boolean,
+        public coef : number,
+
+    ){
+        super(id, email, role);
+    }
 }
